5640714f71b4d1e6b50bfab1eb7bbeaf662b817b
mikepmerritt <mikepmerritt@gmail.com>
2023-04-26 08:08:19 -0400

    added progress bar for tasks as feedback

--- Assets/Scripts/TaskBehavior.cs
+++ Assets/Scripts/TaskBehavior.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class TaskBehavior : MonoBehaviour
 {
-    [SerializeField, Range(0f, 5f)] private float RemainingTime;
+    [SerializeField, Range(0f, 5f)] private float StartingTime, RemainingTime;
     [SerializeField, Range(0, 1000)] private int PointValue;
     [SerializeField] private ScoreManager ScoreManager;
+    [SerializeField] private GameObject ProgressBar;
+
+    protected void Start()
+    {
+        RemainingTime = StartingTime;
+    }
 
     protected void Update()
     {
+        // progress bar
+        float barPercent = RemainingTime / StartingTime;
+        ProgressBar.transform.localScale = new Vector3(barPercent, 1, 1);
+        ProgressBar.transform.localPosition = new Vector3(-(1 - barPercent) / 2f, 0, 0);
+
         if(RemainingTime < 0)
         {
             ScoreManager.AddScore(PointValue);
